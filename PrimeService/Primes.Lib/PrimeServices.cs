41c3570f597b7515793eb4491de02b4959b64afa
Brunoenr02 <brunoancco02@gmail.com>
2025-06-01 23:24:09 -0500

    Actividad02 - Documentacion con DocFX

--- PrimeService/Primes.Lib/PrimeServices.cs
+++ PrimeService/Primes.Lib/PrimeServices.cs
@@ -1,7 +1,15 @@
 namespace Primes.Lib
 {
+    /// <summary>
+    /// Proporciona servicios relacionados con números primos.
+    /// </summary>
     public class PrimeService
     {
+        /// <summary>
+        /// Determina si un número es primo.
+        /// </summary>
+        /// <param name="candidate">El número a evaluar.</param>
+        /// <returns>True si el número es primo; de lo contrario, false.</returns>
         public bool IsPrime(int candidate)
         {
             if (candidate < 2) return false;
